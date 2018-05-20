804eeba9b7e797370a9f689237e5c23fa7fa6f53
alexk111 <alexk111@users.noreply.github.com>
2018-05-20 21:14:55 +0700

    /dist -> gh-pages

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-
-import {initPage} from './modules/page'
-
-initPage()
--- src/modules/currency.js
+++
@@ -1,3 +0,0 @@
-export function satsToBTC (sats) {
-  return sats / 100000000
-}
