05174a23e7dcf290ce28f9d22d5214807c38a794
Alex Kaul <gh@alexkaul.com>
2020-10-30 16:50:42 +0700

    Remove unused scripts

--- src/modules/page.js
+++ src/modules/page.js
@@ -1,107 +1,102 @@
-import $ from 'jquery'
-import {btnToCopyBtn} from './clipboard'
-import {normalizePaymentLink} from './paymentLink'
-import {getAndParsePaymentRequest} from './paymentRequest'
-import {showModal} from './modals'
-import {paymentDataToModalContent} from './paymentModal'
-import {htmlHowToRunLocally} from './howToRunLocally'
-import {htmlHowItWorks} from './howItWorks'
-import {htmlImportantNotice} from './importantNotice'
-import {showDonationScreen, preloadDonationScreen} from './donation'
+import $ from "jquery";
+import { btnToCopyBtn } from "./clipboard";
+import { normalizePaymentLink } from "./paymentLink";
+import { getAndParsePaymentRequest } from "./paymentRequest";
+import { showModal } from "./modals";
+import { paymentDataToModalContent } from "./paymentModal";
+import { htmlHowToRunLocally } from "./howToRunLocally";
+import { htmlHowItWorks } from "./howItWorks";
+import { htmlImportantNotice } from "./importantNotice";
 
-const isLocalApp = (window.location.protocol === 'file:')
+const isLocalApp = window.location.protocol === "file:";
 
-function getParsingState () {
-  return (!!$('#indexForm button').prop('disabled'))
+function getParsingState() {
+  return !!$("#indexForm button").prop("disabled");
 }
 
-function setParsingState (val) {
-  $('#indexForm button').prop('disabled', val)
+function setParsingState(val) {
+  $("#indexForm button").prop("disabled", val);
 }
 
-function initTitle () {
+function initTitle() {
   if (isLocalApp) {
-    $('#app-title').text('DeBitpay (Local)')
-    document.title = 'DeBitpay (Local)'
+    $("#app-title").text("DeBitpay (Local)");
+    document.title = "DeBitpay (Local)";
   }
 }
 
-function initParseButton () {
-  $('#indexForm').submit((evt) => {
-    evt.preventDefault()
+function initParseButton() {
+  $("#indexForm").submit((evt) => {
+    evt.preventDefault();
 
     if (!getParsingState()) {
-      setParsingState(true)
-      const invUrl = $('#indexForm input').val()
+      setParsingState(true);
+      const invUrl = $("#indexForm input").val();
       if (invUrl) {
         getAndParsePaymentRequest(normalizePaymentLink(invUrl))
           .done((data) => {
-            showModal('Payment Request Data', paymentDataToModalContent(data), { size: 'lg' })
-            setParsingState(false)
+            showModal("Payment Request Data", paymentDataToModalContent(data), {
+              size: "lg",
+            });
+            setParsingState(false);
           })
           .fail((err) => {
-            showModal('Error', err)
-            setParsingState(false)
-          })
+            showModal("Error", err);
+            setParsingState(false);
+          });
       } else {
-        showModal('Warning', 'Enter Payment URL or Invoice Page URL to continue')
-        setParsingState(false)
+        showModal(
+          "Warning",
+          "Enter Payment URL or Invoice Page URL to continue"
+        );
+        setParsingState(false);
       }
     }
-  })
+  });
 }
 
-function initHowToRunLocally () {
+function initHowToRunLocally() {
   if (isLocalApp) {
     // hide 'how to run locally' button if local version is opened
-    $('button.how-to-run-locally').remove()
+    $("button.how-to-run-locally").remove();
   } else {
-    $('button.how-to-run-locally').click((evt) => {
-      evt.preventDefault()
-      showModal('How To Run DeBitpay Locally', htmlHowToRunLocally, { size: 'lg' })
-    })
+    $("button.how-to-run-locally").click((evt) => {
+      evt.preventDefault();
+      showModal("How To Run DeBitpay Locally", htmlHowToRunLocally, {
+        size: "lg",
+      });
+    });
   }
 }
 
-function initHowItWorks () {
-  $('button.how-it-works').click((evt) => {
-    evt.preventDefault()
-    showModal('How It Works', htmlHowItWorks, { size: 'lg' })
-  })
+function initHowItWorks() {
+  $("button.how-it-works").click((evt) => {
+    evt.preventDefault();
+    showModal("How It Works", htmlHowItWorks, { size: "lg" });
+  });
 }
 
-function initImportantNotice () {
-  $('button.important-notice').click((evt) => {
-    evt.preventDefault()
-    showModal('Important Notice', htmlImportantNotice, { size: 'lg' })
-  })
+function initImportantNotice() {
+  $("button.important-notice").click((evt) => {
+    evt.preventDefault();
+    showModal("Important Notice", htmlImportantNotice, { size: "lg" });
+  });
 }
 
-function initDonate() {
-  $('button.donate').click(evt => {
-    evt.preventDefault()
-    showDonationScreen()
-  })
-  $('button.donate').mouseover(_ => {
-    preloadDonationScreen()
-  })
+function initFooter() {
+  btnToCopyBtn($("footer .btn-clipboard"));
 }
 
-function initFooter () {
-  btnToCopyBtn($('footer .btn-clipboard'))
+function loadingDone() {
+  $("body > .is-loading").remove();
 }
 
-function loadingDone () {
-  $('body > .is-loading').remove()
-}
-
-export function initPage () {
-  initTitle()
-  initParseButton()
-  initHowToRunLocally()
-  initHowItWorks()
-  initImportantNotice()
-  initDonate()
-  initFooter()
-  loadingDone()
+export function initPage() {
+  initTitle();
+  initParseButton();
+  initHowToRunLocally();
+  initHowItWorks();
+  initImportantNotice();
+  initFooter();
+  loadingDone();
 }
